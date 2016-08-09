3a0c055a842ac95163b8833983bfcacdec99a32e
Eliasib García <eliasib.lv.12@gmail.com>
2016-08-09 13:35:14 +0100

    Testing integration with Travis-CI.

--- config/database.php
+++ config/database.php
@@ -79,6 +79,18 @@ return [
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
+		],
+
+		'testing' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_TEST_HOST', 'localhost'),
+			'database'  => env('DB_TEST_DATABASE', 'sigeo'),
+			'username'  => env('DB_TEST_USERNAME', 'homestead'),
+			'password'  => env('DB_TEST_PASSWORD', 'secret'),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
 		]
 
 	],
