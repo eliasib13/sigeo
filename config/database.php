0daf284563543fad1b16656379e72f6ba6a6ec77
Eliasib García <eliasib.lv.12@gmail.com>
2016-08-11 09:09:20 +0100

    Fixde typo on Azure DB connection

--- config/database.php
+++ config/database.php
@@ -95,11 +95,11 @@ return [
 		
 		'azure' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost')),
-			'port'      => env('DB_PORT', 3306)),
-			'database'  => env('DB_DATABASE', 'forge')),
-			'username'  => env('DB_USERNAME', 'forge')),
-			'password'  => env('DB_PASSWORD', '')),
+			'host'      => env('DB_HOST', 'localhost'),
+			'port'      => env('DB_PORT', 3306),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
