6f31d26d339e99bb3445f11d5ef2091ee2a6da2f
Eliasib García <eliasib.lv.12@gmail.com>
2016-08-11 09:01:09 +0100

    Added Azure DB connection

--- config/database.php
+++ config/database.php
@@ -91,6 +91,19 @@ return [
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
 			'strict'    => false,
+		],
+		
+		'azure' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', 'localhost')),
+			'port'      => env('DB_PORT', 3306)),
+			'database'  => env('DB_DATABASE', 'forge')),
+			'username'  => env('DB_USERNAME', 'forge')),
+			'password'  => env('DB_PASSWORD', '')),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
 		]
 
 	],
