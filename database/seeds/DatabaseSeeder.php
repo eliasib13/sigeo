71592b51f3ede6c7edf76996500bd920bc320587
Eliasib García <eliasib.lv.12@gmail.com>
2016-08-12 17:40:58 +0100

    Setting database seed (users, exams)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Webpatser\Uuid\Uuid;
+use App\User;
+use App\Exam;
 
 class DatabaseSeeder extends Seeder {
 
@@ -14,9 +19,93 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		$this->call('UserTableSeeder');
+		DB::table('exams')->delete();
+		DB::table('users')->delete();
 
-		$this->command->info('User table seeded!');
+		$this->call('UserTableSeeder');
+		$this->call('ExamTableSeeder');
+
 	}
 
+}
+
+/*
+██╗   ██╗███████╗███████╗██████╗
+██║   ██║██╔════╝██╔════╝██╔══██╗
+██║   ██║███████╗█████╗  ██████╔╝
+██║   ██║╚════██║██╔══╝  ██╔══██╗
+╚██████╔╝███████║███████╗██║  ██║
+╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═╝
+*/
+
+
+class UserTableSeeder extends Seeder
+{
+	public function run()
+	{
+		$users = [
+			[
+				'name' => 'Administrador',
+				'email' => 'admin@email.com',
+				'password' => Hash::make('admin'),
+				'uuid' => strval(Uuid::generate(4))
+			],
+			[
+				'name' => 'Usuario Test',
+				'email' => 'usertest@email.com',
+				'password' => Hash::make('test'),
+				'uuid' => strval(Uuid::generate(4))
+			]
+		];
+
+		foreach ($users as $user) {
+			User::Create($user);
+		}
+	}
+}
+
+/*
+███████╗██╗  ██╗ █████╗ ███╗   ███╗
+██╔════╝╚██╗██╔╝██╔══██╗████╗ ████║
+█████╗   ╚███╔╝ ███████║██╔████╔██║
+██╔══╝   ██╔██╗ ██╔══██║██║╚██╔╝██║
+███████╗██╔╝ ██╗██║  ██║██║ ╚═╝ ██║
+╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝
+*/
+
+class ExamTableSeeder extends Seeder
+{
+	public function run()
+	{
+		$exams = [
+			[
+				'uuid' => strval(Uuid::generate(4)),
+				'name' => 'Examen 1 de Administrador',
+				'creatorId' => User::where('email', 'admin@email.com')->first()->id,
+				'passingScore' => 5
+			],
+			[
+				'uuid' => strval(Uuid::generate(4)),
+				'name' => 'Examen 2 de Administrador',
+				'creatorId' => User::where('email', 'admin@email.com')->first()->id,
+				'passingScore' => 7
+			],
+			[
+				'uuid' => strval(Uuid::generate(4)),
+				'name' => 'Examen 1 de Usuario Test',
+				'creatorId' => User::where('email', 'usertest@email.com')->first()->id,
+				'passingScore' => 4
+			],
+			[
+				'uuid' => strval(Uuid::generate(4)),
+				'name' => 'Examen 2 de Usuario Test',
+				'creatorId' => User::where('email', 'usertest@email.com')->first()->id,
+				'passingScore' => 6.5
+			]
+		];
+
+		foreach ($exams as $exam) {
+			Exam::Create($exam);
+		}
+	}
 }
