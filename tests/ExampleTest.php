756623a9e22f0936d1d87e50d1a53c0f238ac3f2
Eliasib García <eliasib.lv.12@gmail.com>
2016-09-08 21:32:30 +0100

    Added unit test of "Hello" test route.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,4 +14,16 @@ class ExampleTest extends TestCase {
 		$this->assertEquals(200, $response->getStatusCode());
 	}
 
+	/**
+	 * Trying new Route example.
+	 *
+	 * @return void
+	 */
+	public function testHelloRoute()
+	{
+		$response = $this->call('GET', '/hello/testing');
+
+		$this->assertEquals('Hello testing!', $response->getContent());
+	}
+
 }
