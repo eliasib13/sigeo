975dba0c912cd0843cb94ca69fa6b8315d56dec3
Eliasib García <eliasib.lv.12@gmail.com>
2016-09-19 08:59:56 +0100

    Fixed SiGEO logo url redirect.

--- resources/views/header-bar.blade.php
+++ resources/views/header-bar.blade.php
@@ -1,5 +1,5 @@
 <div class="header-bar">
-    <h1 class="ui header"><a href="/sigeo/dashboard">SiGEO</a></h1>
+    <h1 class="ui header"><a href="{{ url('login') }}">SiGEO</a></h1>
     <div class="header-bar-buttons">
         <div class="button"><i class="icon user"></i>Test User<i class="icon caret down"></i></div>
     </div>
