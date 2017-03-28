35ba2daf2746f6ef2b9354449dbbe37fea494e85
Eliasib García <eliasib.lv.12@gmail.com>
2017-03-28 09:03:43 +0100

    Added Web.config for Azure deployment.
    - Fixed generic routes of static assets (css, js)

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -4,17 +4,17 @@
     <meta charset="utf-8">
     <title>SiGEO</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.css"/>
-    <link rel="stylesheet" href="/sigeo/css/header-bar.css" />
-    <link rel="stylesheet" href="/sigeo/css/common.css" />
-    <link rel="stylesheet" href="/sigeo/css/login/login.css" />
-    <link rel="stylesheet" href="/sigeo/css/dashboard/dashboard.css" />
-    <link rel="stylesheet" href="/sigeo/css/room/details.css" />
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/css/header-bar.css" />
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/css/common.css" />
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/css/login/login.css" />
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/css/dashboard/dashboard.css" />
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/css/room/details.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.js"></script>
 </head>
 <body>
     @yield('content')
 
-    <script src="/sigeo/js/init-components.js"></script>
+    <script src="{{ env('APP_URL') }}/js/init-components.js"></script>
 </body>
 </html>
